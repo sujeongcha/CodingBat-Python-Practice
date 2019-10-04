8cebafbbcdd69675a7ce977a87a494ae851f8edb
Sue Sujeong Cha <39662008+sujeongcha@users.noreply.github.com>
2019-10-04 18:58:30 -0400

    monkey_trouble.py

--- Warmup-1/2. monkey_trouble.py
+++ Warmup-1/2. monkey_trouble.py
@@ -1,4 +1,5 @@
-#We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+#We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. 
+#We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 
 #monkey_trouble(True, True) → True
 #monkey_trouble(False, False) → True
